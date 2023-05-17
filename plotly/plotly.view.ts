c3471db8bcb2792207db4c34cc9d5aca94f4eaec
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-17 23:00:30 +0300

    revert

--- plotly/plotly.view.ts
+++ plotly/plotly.view.ts
@@ -8,21 +8,25 @@ namespace $.$$ {
 		}
 
 		@ $mol_mem
-		sub(): HTMLElement[] {
-			if (!this.view_rect()) return []
-			
+		render() {
+			if (!this.view_rect()) return
 			const { width, height } = this.view_rect()!
+			
 			const plotly_root = document.createElement('div')
 			plotly_root.style.position = 'absolute' //otherwise plotly_root prevents dom_node from resizing
 			
-			$mol_wire_async( $lib_plotly.all() ).react(
+			const promise = $lib_plotly.all().react(
 				plotly_root,
 				this.data(), 
 				{ ...this.layout(), width, height },
 				this.plot_options(),
 			)
-
-			return [ plotly_root ]
+				
+			const dom_node = this.dom_node_actual() as HTMLElement
+			promise.then( ( plotly_root: HTMLElement )=> {
+				dom_node.replaceChildren( plotly_root )
+				this.subscribe_events() 
+			})
 		}
 
 	}
