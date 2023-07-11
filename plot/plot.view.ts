1b35c86a6e4de94af746ab7a933b31f15a0d0e35
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-07-11 23:37:38 +1100

    notify

--- plot/plot.view.ts
+++ plot/plot.view.ts
@@ -3,7 +3,7 @@ namespace $.$$ {
 	export class $mpds_visavis_plot extends $.$mpds_visavis_plot {
 
 		@ $mol_action
-		fetch_plot_json( request: RequestInfo ){
+		fetch_plot_json( request: RequestInfo | null ){
 			if ( request == null ) return null
 
 			const json = $mol_fetch.json( request ) as any
@@ -23,6 +23,14 @@ namespace $.$$ {
 		@ $mol_mem
 		json_cmp() {
 			return this.fetch_plot_json( this.json_cmp_request() )
+		}
+
+		@ $mol_mem
+		json_cmp_request( next?: string | null ) {
+			if ( next === null && $mol_wire_probe( ()=> this.json_cmp_request() ) === null ) {
+				this.notify( 'Comparison was reset' )
+			}
+			return next ?? null
 		}
 
 		@ $mol_mem
@@ -65,6 +73,11 @@ namespace $.$$ {
 
 		@ $mol_action
 		on_fixel_checked( checked: boolean ) { }
+
+		@ $mol_action
+		notify( msg: string ) {
+			alert( msg )
+		}
 
 	}
 
