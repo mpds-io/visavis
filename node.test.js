3a45cee190c67387fd5036bcef1bd19ef9a51c60
blokhin <blokhin@users.noreply.github.com>
2023-10-06 07:16:14 +0000

    Deploying to gh-pages from @ mpds-io/visavis@4cf7973488f41e39a03104b677be2510af13b60f 🚀

--- node.test.js
+++ node.test.js
@@ -19011,6 +19011,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();
--- web.mjs
+++ web.mjs
@@ -18409,6 +18409,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();
