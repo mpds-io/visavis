d4e8224929fecc6239278d03be4aebc086665bc8
Evgeny Blokhin <eb@tilde.pro>
2025-01-14 14:57:14 +0100

    Change fullscreen setup

--- plot/plot.view.css.ts
+++ plot/plot.view.css.ts
@@ -2,16 +2,6 @@ namespace $.$$ {
 
 	$mol_style_define( $mpds_visavis_plot, {
 
-		Fullscreen: {
-			position: 'absolute',
-			right: 0,
-			Icon: {
-				width: '1.5rem',
-				height: '1.5rem',
-			},
-			zIndex: 5,
-		},
-		
 		Demo_warn: {
 			position: 'absolute',
 			left: 0,
